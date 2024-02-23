c4b7a9aa3a70f89c1e1e7d9e8cf2f6b43aa89fc1
Ammar <ammar.yassr.33@gmail.com>
2024-02-24 00:08:48 +0200

    Add solution for LeetCode problem 349

---
+++ LeetCode/349. Intersection of Two Arrays.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  const freq1 = new Map();
+  const freq2 = new Map();
+
+  for (const n of nums1) freq1.set(n, 1);
+  for (const n of nums2) freq2.set(n, 1);
+
+  let result = [];
+  for (const [n, f] of freq2) {
+    if (freq1.has(n)) result.push(n);
+  }
+
+  return result;
+};
+
+// console.log(intersection([1, 2, 2, 1], [2, 2])); // [2]
+// console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4])); // [9,4]
