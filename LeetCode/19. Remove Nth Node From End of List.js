3b81639e13edecd8591c7e78a699f14b29b26670
Ammar <ammar.yassr.33@gmail.com>
2024-03-03 16:59:19 +0200

    Add ListNode constructor

--- LeetCode/19. Remove Nth Node From End of List.js
+++ LeetCode/19. Remove Nth Node From End of List.js
@@ -1,3 +1,8 @@
+// function ListNode(val, next) {
+//   this.val = val === undefined ? 0 : val;
+//   this.next = next === undefined ? null : next;
+// }
+
 /**
  * @param {ListNode} head
  * @param {number} n
